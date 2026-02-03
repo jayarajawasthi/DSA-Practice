12d736847200d44de193914aaa828b76d0aa30a2
Jayaraj Awasthi <awasthijayaraj11@gmail.com>
2026-02-03 17:32:06 +0545

    add test files

--- Test/test1/test1.cpp
+++ Test/test1/test1.cpp
@@ -1,15 +1,190 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printName(int i, int number){
-	if (i > number) return;
-	cout<<"Hello"<<endl;
-	printName(i+1, number);
+// int main (){
+// 	int n;
+// 	cin >> n;
+
+// 	for(int i = 1; i <= n; i ++){
+// 		for (int j = 1; j <= n ; j++){
+// 			cout<<"*"<<"	 ";
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+// int main() {
+// 	int n ; 
+// 	cin >> n;
+	
+// 	for (int i = 0; i < n; i++){
+// 		char ch = 'A';
+// 		for(int j = 0; j < n ; j++ ){
+// 			cout<<ch << " ";
+// 			ch += 1;
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+
+// int main() {
+// 	int n ; 
+// 	cin >> n ;
+// 	int x = 1;
+// 	for(int i = 0 ; i < n; i++) {
+		
+// 		for(int j = 0; j < n; j++ ){
+// 			cout<<x << " 	";
+// 			x += 1;
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+// int main() {
+// 	int n; 
+// 	cin >> n;
+// 	char ch = 'A';
+
+// 	for(int i = 0; i < n ; i++) {
+		
+// 		for( int j = 0; j < n; j++) {
+// 			cout<<ch<<" ";
+// 			ch += 1;
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+
+// int main () {
+// 	int n; 
+// 	cin >> n;
+
+// 	for (int i = 0; i < n; i++ ){
+// 		for(int j = 0; j <= i; j++ ) {
+// 			cout<<"*"<< " ";
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+
+// int main () {
+// 	int n ;
+// 	cin >> n;
+
+	
+	
+// 	for(int i = 0 ; i < n; i ++ ){
+
+// 		for(int j = 0; j <= i; j++){
+
+// 			cout<<i+1<<" ";
+			
+// 		}
+
+// 		cout<<endl;
+// 	}
+// }
+
+
+
+// int main () {
+// 	int n ;
+// 	cin >> n;
+
+// 	char ch = 'A';
+
+// 	for(int i = 0; i < n ; i++) {
+// 		for(int j = 0; j <= i; j++) {
+// 			cout<< char(ch + i)<<" ";
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+
+// int main() {
+// 	int n; 
+// 	cin >> n;
+
+// 	for(int i = 0; i < n; i++){
+// 		for(int j =1; j<= i; j++) {
+// 			cout<<j<<" ";
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+
+
+
+// int main () {
+// 	int n; 
+// 	cin >> n;
+
+// 	for(int i = 1; i <= n; i ++){
+// 		for(int j = i; j >= i; j--){
+// 			cout<<j<<" ";
+// 		}
+// 		cout<<endl;
+// 	}
+// }
+
+
+// int main() {
+// 	int n;
+// 	cin >> n;
+
+// 	for(int i = 0; i < n; i ++) {
+// 		for (int j = 0; j <= i; j ++){
+// 			cout<<" * ";
+// 		}
+// 		cout<<endl;
+// 	}
+// 		for(int i = 0; i < n; i ++) {
+// 		for (int j = 0; j <= i; j ++){
+// 			cout<<" * ";
+// 		}
+// 		cout<<endl;
+// 	}
+// 	for(int i = 0; i < n; i++){
+// 		cout<<" * "<<endl;
+// 	}
+// }
+
+
+
+
+// int main() {
+// 	int n;
+// 	cin >> n;
+
+// 	for(int i = 0; i < n; i++) {
+// 		for(int j = 0 ; j < i; j++){
+// 			cout<<" ";
+// 		}
+// 		for(int j = 0; j < n - i; j++){
+// 			cout<<(i+1);
+// 		}
+// 		cout<<endl;
+// 	}
+
+// }
+
+
+
+
+int printHello() {
+	cout<<"hello"<<endl;
+	return 2;
 }
 
-int main()
-{
-	int n;
-	cin >> n;
-	printName(1, n);
+int main() {
+	cout<<printHello();
+
+
+	return 0;
 }
